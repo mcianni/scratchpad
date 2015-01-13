795c514cf105ac46d472c748ce6d832c65f735ad
mcianni <mcianni@gmail.com>
2015-01-13 14:14:10 -0500

    Only scratch text/html

--- lib/scratchpad/middleware.rb
+++ lib/scratchpad/middleware.rb
@@ -7,7 +7,7 @@ module Scratchpad
     def call env
       scratchpad = Scratchpad::Page.new(env)
       status, headers, response = @app.call(env)
-      if headers["Content-Type"].include? "text/html"
+      if headers["Content-Type"] && headers["Content-Type"].include?("text/html")
         [status, headers, [response.body + scratchpad.to_html]]
       else
         [status, headers, response]
